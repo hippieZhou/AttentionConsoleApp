b2765aa2e9b524c4daa857e24cb076950363129a
hippieZhou <hippiezhou@outlook.com>
2018-09-28 22:36:17 +0800

    update

--- Attention.DAL/AttentionDbContext.cs
+++ Attention.DAL/AttentionDbContext.cs
@@ -7,6 +7,7 @@ namespace Attention.DAL
     public class AttentionDbContext : DbContext
     {
         public DbSet<Bing> Bings { get; set; }
+        public DbSet<Ioliu> Iolius { get; set; }
         public AttentionDbContext(DbContextOptions options) : base(options)
         {
             Database.EnsureCreated();
