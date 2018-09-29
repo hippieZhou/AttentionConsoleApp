c01ec9f550c219420ee312e2377baf0d4dd431d0
zhouqiang <zhouqiang@4dogs.cn>
2018-09-29 18:24:44 +0800

    update

--- Attention.App.Web/Startup.cs
+++ Attention.App.Web/Startup.cs
@@ -7,11 +7,21 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using DotVVM.Framework.Hosting;
+using Attention.BLL.Clients;
+using Attention.DAL;
+using Attention.BLL.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Attention.App.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -21,6 +31,10 @@ namespace Attention.App.Web
             services.AddAuthorization();
             services.AddWebEncoders();
             services.AddDotVVM();
+
+            services.AddHttpClient<BingClient>();
+            services.AddDbContext<AttentionDbContext>(options => { options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); });
+            services.AddSingleton<BingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Attention.App.Web/ViewModels/BingViewModel.cs
+++ Attention.App.Web/ViewModels/BingViewModel.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using Attention.BLL.Services;
 using DotVVM.Framework.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Attention.App.Web.ViewModels
 {
@@ -11,6 +14,11 @@ namespace Attention.App.Web.ViewModels
         public BingViewModel()
         {
             Title = "Bing";
+        }
+
+        public void Submit()
+        {
+            Trace.WriteLine(DateTime.Now);
         }
     }
 }
