a1b942504526c19e6eda1b8542dde1c6ee5dd049
hippie <zhouqiang@4dogs.cn>
2018-10-17 14:15:37 +0800

    add unit test

---
+++ Attention.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Attention.Tests
+{
+    //https://www.cnblogs.com/willick/p/aspnetcore-unit-tests-with-xunit.html
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
